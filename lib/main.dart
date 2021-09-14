53a94b42b35d203b95f8098ed3a7ad761f082b45
johnny <johnny90347@gmail.com>
2021-09-14 12:12:25 +0800

    新增啟動頁

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
+/// 啟動頁
+class SplashView extends StatefulWidget {
+  final VoidCallback completedCallBack;
+
+  const SplashView({required this.completedCallBack, Key? key}) : super(key: key);
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    _animation = Tween(
+      begin: 1.0,
+      end: 0.0,
+    ).animate(_controller)
+      ..addStatusListener((AnimationStatus status) {
+        if (status == AnimationStatus.completed) {
+          widget.completedCallBack();
+        }
+      });
+
+    /// 1.3 秒後,開始漸變淡
+    Future.delayed(Duration(milliseconds: 2000), () {
+      _controller.forward();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: FadeTransition(
+        opacity: _animation,
+        child: Image(
+          image: AssetImage("assets/images/splash_bg.jpeg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
 class WebViewScreen extends StatefulWidget {
   const WebViewScreen({Key? key}) : super(key: key);
 
@@ -31,6 +91,26 @@ class _WebViewScreenState extends State<WebViewScreen> {
 
   double slimmingSize = 0.0;
 
+  /// close splash
+  bool closeSplashView = false;
+
+  /// start Loading webview
+  bool startLoadingWebView = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance?.addPostFrameCallback((_) async {
+      /// 0.5秒後,開始載入WebView
+      Future.delayed(Duration(milliseconds: 400), () {
+        setState(() {
+          startLoadingWebView = true;
+        });
+      });
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final appBarHeight = kToolbarHeight;
@@ -41,28 +121,45 @@ class _WebViewScreenState extends State<WebViewScreen> {
       body: OrientationBuilder(
         builder: (context, orientation) => Container(
           margin: EdgeInsets.only(
-            top:  orientation == Orientation.portrait ? slimmingSize : 0,
-            left: orientation == Orientation.landscape ? slimmingSize : 0,
-            right: orientation == Orientation.landscape ? slimmingSize : 0,
-            bottom: 0
-          ),
+              top: orientation == Orientation.portrait ? slimmingSize : 0,
+              left: orientation == Orientation.landscape ? slimmingSize : 0,
+              right: orientation == Orientation.landscape ? slimmingSize : 0,
+              bottom: 0),
           color: Colors.black,
           width: double.infinity,
           height: double.infinity,
           child: Stack(
             key: _parentKey,
             children: [
+              /// webView
               Positioned.fill(
-                child: InAppWebView(
-                  initialUrlRequest: URLRequest(url: Uri.parse(_mobileUrl)),
-                  initialOptions: InAppWebViewGroupOptions(
-                      crossPlatform: InAppWebViewOptions(
-                    // cacheEnabled: false,
-                    supportZoom: false,
-                  )),
-                  onWebViewCreated: (InAppWebViewController controller) {},
-                ),
+                child: startLoadingWebView
+                    ? InAppWebView(
+                        initialUrlRequest: URLRequest(url: Uri.parse(_mobileUrl)),
+                        initialOptions: InAppWebViewGroupOptions(
+                            crossPlatform: InAppWebViewOptions(
+                          // cacheEnabled: false,
+                          supportZoom: false,
+                        )),
+                        onWebViewCreated: (InAppWebViewController controller) {},
+                      )
+                    : SizedBox(),
               ),
+
+              /// 啟動頁覆蓋
+              if (!closeSplashView)
+                Positioned.fill(
+                  child: SplashView(
+                    completedCallBack: () {
+                      /// 動畫做完了
+                      Future.delayed(Duration(milliseconds: 300), () {
+                        setState(() {
+                          closeSplashView = true;
+                        });
+                      });
+                    },
+                  ),
+                ),
             ],
           ),
         ),
